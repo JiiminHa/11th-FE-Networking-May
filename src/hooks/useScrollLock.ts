1e6ee26d23a4e87ec79f367d8545707af57321e8
JiiminHa <tracygkwlals@sookmyung.ac.kr>
2025-05-21 10:35:06 +0900

    feat: 위치 추가 모달 UI 및 기능 구현

---
+++ src/hooks/useScrollLock.ts
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+export function useScrollLock() {
+  useEffect(() => {
+    const originalStyle = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = originalStyle;
+    };
+  }, []);
+}
