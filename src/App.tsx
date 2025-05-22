9f56f805aafc17d677fa6b254e677c391092fe9d
JiiminHa <tracygkwlals@sookmyung.ac.kr>
2025-05-23 05:48:15 +0900

    feat: 위치 미선택 시 노출되는 빈 페이지 UI 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
+// App.tsx
+import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import WeatherMainCard from "./components/WeatherMainCard";
 import HourlyForecastSection from "./components/HourlyForecastSection";
 import WeeklyForecastSection from "./components/WeeklyForecastSection";
+import NoLocationSelected from "./components/NoLocationSelected";
+import type { LocationItem } from "./types/location";
 
 function App() {
+  const [selectedLocation, setSelectedLocation] = useState<LocationItem | null>(
+    null
+  );
+
   return (
     <div className="flex min-h-screen">
-      <Sidebar />
+      <Sidebar onSelectLocation={setSelectedLocation} />
 
-      <main className="flex flex-1 px-[296px] py-[75.5px] flex-col gap-[24px] items-center justify-center ">
-        <WeatherMainCard />
-        <HourlyForecastSection />
-        <WeeklyForecastSection />
+      <main className="flex flex-1 px-[296px] py-[75.5px] flex-col gap-[24px] items-center justify-center">
+        {selectedLocation ? (
+          <>
+            <WeatherMainCard location={selectedLocation} />
+            <HourlyForecastSection locationId={selectedLocation.id} />
+            <WeeklyForecastSection locationId={selectedLocation.id} />
+          </>
+        ) : (
+          <NoLocationSelected />
+        )}
       </main>
     </div>
   );
