6f36f94ec1df4c0c564611b7da6b1dafd0663b05
JiiminHa <tracygkwlals@sookmyung.ac.kr>
2025-05-23 12:23:11 +0900

    feat: 시간별 날씨 API 연동 및 UI 데이터 연결

--- src/components/HourlyForecastCard.tsx
+++ src/components/HourlyForecastCard.tsx
@@ -1,12 +1,12 @@
 import type { HourlyForecast } from "../types/weather";
 
 export default function HourlyForecastCard(props: HourlyForecast) {
-  const { time, icon, temperature } = props;
+  const { hour, condition, temperature } = props;
 
   return (
     <div className="flex flex-col items-center w-[48px] gap-2">
-      <img src={icon} alt="weather icon" className="w-10 h-10" />
-      <span className="text-[12px] text-[#A4A4A4]">{time}</span>
+      <img src={condition} alt="weather condition" className="w-10 h-10" />
+      <span className="text-[12px] text-[#A4A4A4]">{hour}</span>
       <span className="text-[12px] text-[#292E2E] ">{temperature}</span>
     </div>
   );
--- src/components/HourlyForecastSection.tsx
+++ src/components/HourlyForecastSection.tsx
@@ -1,49 +1,53 @@
-import HourlyForecastCard from "./HourlyForecastCard";
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+import type { HourlyForecast } from "../types/weather";
+import { getWeatherIconPath } from "../utils/weatherIcon";
+export default function HourlyForecastSection({
+  locationId,
+}: {
+  locationId: number;
+}) {
+  const [hourlyData, setHourlyData] = useState<HourlyForecast[]>([]);
 
-const hourlyData = [
-  { time: "03시", icon: "/icons/Clouds.png", temperature: "8°" },
-  { time: "04시", icon: "/icons/Clouds.png", temperature: "8°" },
-  { time: "05시", icon: "/icons/Clouds.png", temperature: "9°" },
-  { time: "06시", icon: "/icons/Clouds.png", temperature: "10°" },
-  { time: "07시", icon: "/icons/Sun.png", temperature: "11°" },
-  { time: "08시", icon: "/icons/Sun.png", temperature: "12°" },
-  { time: "09시", icon: "/icons/Sun.png", temperature: "13°" },
-  { time: "10시", icon: "/icons/Sun.png", temperature: "14°" },
-  { time: "11시", icon: "/icons/Sun.png", temperature: "15°" },
-  { time: "12시", icon: "/icons/Sun.png", temperature: "15°" },
-  { time: "13시", icon: "/icons/Sun.png", temperature: "15°" },
-  { time: "14시", icon: "/icons/Sun.png", temperature: "15°" },
-];
+  useEffect(() => {
+    const fetchHourlyData = async () => {
+      try {
+        const res = await api.get(
+          `/weather/hourly-status?locationId=${locationId}`
+        );
+        setHourlyData(res.data.results);
+      } catch (err) {
+        console.error("시간별 날씨 정보 불러오기 실패:", err);
+      }
+    };
 
-export default function HourlyForecastSection() {
+    fetchHourlyData();
+  }, [locationId]);
+
   return (
-    <section className="w-full max-w-[1328px] bg-white rounded-[16px] border-[1px] border-[#F2F2F2] shadow-[0_0_8px_2px_rgba(0,0,0,0.1)] p-6 flex flex-col gap-6">
+    <section className="w-full max-w-[1328px] bg-white rounded-[16px] border border-[#F2F2F2] shadow-[0_0_8px_2px_rgba(0,0,0,0.1)] p-6 flex flex-col gap-6">
       <h3 className="text-[20px] font-bold">시간별 현황</h3>
-      <div className="flex flex-col gap-2 px-6 py-3">
-        <div className="overflow-x-auto scrollbar-hide">
-          <div className="relative flex gap-4 w-max">
-            <div className="absolute top-1/2 left-[20px] right-[20px] h-[2px] bg-[#d6d6d6] z-0" />
-            {hourlyData.map((_, idx) => (
-              <div
-                key={idx}
-                className="w-[48px] flex justify-center z-10 shrink-0"
-              >
-                <div className="w-2 h-2 bg-[#d6d6d6] rounded-full" />
-              </div>
-            ))}
-          </div>
-        </div>
 
-        <div className="flex gap-4 overflow-x-auto scrollbar-hide">
-          {hourlyData.map((item) => (
-            <HourlyForecastCard
-              key={item.time}
-              time={item.time}
-              icon={item.icon}
-              temperature={item.temperature}
+      {/* 시간별 카드 리스트 */}
+      <div className="relative flex justify-between w-full px-6 py-3">
+        {hourlyData.map((item) => (
+          <div
+            key={item.hour}
+            className="flex flex-col items-center gap-2 w-[48px] z-10"
+          >
+            <div className="w-2 h-2 bg-[#d6d6d6] rounded-full" />
+            {/* 선 (가로선) */}
+            <div className="absolute top-[16px] left-[24px] right-[22px] h-[2px] bg-[#d6d6d6] z-0" />
+            <img
+              src={`/icons/${getWeatherIconPath(item.condition, item.hour)}`}
+              alt={item.condition}
             />
-          ))}
-        </div>
+            <span className="text-xs text-[#A4A4A4]">{item.hour}</span>
+            <span className="text-xs text-[#292E2E]">
+              {Math.round(item.temperature)}°
+            </span>
+          </div>
+        ))}
       </div>
     </section>
   );
